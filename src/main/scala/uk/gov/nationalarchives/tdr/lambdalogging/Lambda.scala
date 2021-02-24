47d0ffa8b4b0e223a1cf0f52f468538cb2c5242a
Suzanne Hamilton <suzanne.hamilton@nationalarchives.gov.uk>
2021-02-24 16:35:44 +0000

    Add tag to log message

--- src/main/scala/uk/gov/nationalarchives/tdr/lambdalogging/Lambda.scala
+++ src/main/scala/uk/gov/nationalarchives/tdr/lambdalogging/Lambda.scala
@@ -4,6 +4,8 @@ import java.util
 
 import com.amazonaws.services.lambda.runtime.{Context, RequestHandler}
 import com.typesafe.scalalogging.Logger
+import org.slf4j.MarkerFactory
+
 
 class Lambda extends RequestHandler[java.util.Map[String, String], String] {
   val logger: Logger = Logger[Lambda]
@@ -14,6 +16,9 @@ class Lambda extends RequestHandler[java.util.Map[String, String], String] {
     logger.error("Error-level hello")
 
     logger.error("How are errors logged?", new RuntimeException("just testing how a stacktrace is logged"))
+
+    val marker = MarkerFactory.getMarker("markerName")
+    logger.info(marker, "some info message with a marker")
 
     "This is the Lambda return value"
   }
